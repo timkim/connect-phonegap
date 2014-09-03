c3fd1adf75b0bd120e01a0aa02a0eb97f55b6a1b
Tim Kim <timk@adobe.com>
2014-09-03 15:10:29 -0700

    Trying to get autoreload to work

--- res/middleware/autoreload.js
+++ res/middleware/autoreload.js
@@ -4,7 +4,51 @@
 //
 (function() {
 
-    var url = 'http://' + document.location.host + '/__api__/autoreload';
+    var url = 'http://' + '10.58.135.131:3000' + '/__api__/autoreload';
+
+    function downloadZip(){
+        //alert(typeof zip);
+        window.requestFileSystem(
+            LocalFileSystem.PERSISTENT,
+            0,
+            function(fileSystem) {
+                //alert('file system');
+                var fileTransfer = new FileTransfer();
+                var uri = encodeURI('http://10.58.135.130:3000' + '/__api__/zip');
+    
+                fileTransfer.download(
+                    uri,
+                    fileSystem.root.toURL() + 'app.zip',
+                    function(entry) {
+                        //alert('got file');
+                        console.log("download complete: " + entry.toURL());
+                        
+                        zip.unzip(fileSystem.root.toURL() + 'app.zip', fileSystem.root.toURL() +'/app', function(statusCode) {
+                            if (statusCode === 0) {
+                                alert('unzipped');
+                                console.log('[fileUtils] successfully extracted the update payload');
+                                //window.location.href = fileSystem.root.toURL() +'/app/index.html'; 
+                                window.location.reload();
+                            }
+                            else {
+                                console.error('[fileUtils] error: failed to extract update payload');
+                                console.log(zipPath, dirPath);
+                            }
+                        });
+                    },
+                    function(error) {
+                        console.log("download error source " + error.source);
+                        console.log("download error target " + error.target);
+                        console.log("upload error code" + error.code);
+                    },
+                    false
+                );        
+            },
+            function(e) {
+                callback(e);
+            }
+        );
+    }
 
     function postStatus(){
         var xhr = new XMLHttpRequest;
@@ -26,7 +70,9 @@
                 var response = JSON.parse(this.responseText);
                 if (response.content.outdated) {
                     postStatus();
-                    window.location.reload();
+                    downloadZip();
+                    //window.location.reload();
+                    // for content sync - initiate file transfer then reload
                 }
             }
         }
@@ -34,6 +80,5 @@
     }
 
     setInterval(checkForReload, 1000 * 3);
-
 })(window);
 </script>
